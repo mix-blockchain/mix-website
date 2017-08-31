9de11251e583a3814d9628baf640e9d1d5a19990
Jonathan Brown <jbrown@link-blockchain.org>
2017-08-31 17:19:40 +0700

    Switch to port 80.

--- backend.js
+++ backend.js
@@ -5,6 +5,6 @@ var app = express();
 
 app.use(express.static('public'));
 
-app.listen(3001, function () {
-  console.log('Link Blockchain Revenue Status app listening on port 3001!');
+app.listen(80, function () {
+  console.log('LINK Blockchain Website listening on port 80');
 })
