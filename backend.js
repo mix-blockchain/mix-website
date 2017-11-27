2c4999f5309936b304ae02eb53214603d11becd0
Jonathan Brown <jbrown@mix-blockchain.org>
2017-11-27 14:19:20 +0700

    Update logo.

--- backend.js
+++ backend.js
@@ -3,8 +3,13 @@
 var express = require('express');
 var app = express();
 
+app.get(/.svgz/, function(req, res, next) {
+res.set({'Content-Encoding': 'gzip'});
+next();
+});
+
 app.use(express.static('public'));
 
 app.listen(81, function () {
   console.log('MIX Blockchain Website listening on port 81');
-})
+});
